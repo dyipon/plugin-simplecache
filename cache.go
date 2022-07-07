99e83a2822f24a0d336ce4d970d232e7cbb6ff5f
dy <dy@ipon.hu>
2022-07-07 10:34:14 +0200

    disable log

--- cache.go
+++ cache.go
@@ -151,7 +151,7 @@ func (m *cache) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (m *cache) cacheable(r *http.Request, w http.ResponseWriter, status int) (time.Duration, bool) {
 	// log.Printf("cacheable status: %d", status)
 
-	log.Printf("cacheable status: %d, rawquery:, %s", status, r.URL.RawQuery)
+	// log.Printf("cacheable status: %d, rawquery:, %s", status, r.URL.RawQuery)
 
 	if len(r.URL.RawQuery) > 1 {
 		return 0, false
